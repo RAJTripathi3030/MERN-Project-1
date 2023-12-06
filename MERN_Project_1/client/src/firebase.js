8746f6b1250532d3f6e50fe5f0ab2b9dd4c7b09e
raj tripathi <tripathiraj3030@gmail.com>
2023-12-06 13:53:06 +0530

    first commit

---
+++ MERN_Project_1/client/src/firebase.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNf-kkdcFwa80zPIUbOaNHwWMkRq9o_xg",
+  authDomain: "randint-88673.firebaseapp.com",
+  projectId: "randint-88673",
+  storageBucket: "randint-88673.appspot.com",
+  messagingSenderId: "517618240439",
+  appId: "1:517618240439:web:4eafc6fe831a1399bb7632"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
